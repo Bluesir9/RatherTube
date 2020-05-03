e674e100fc787c158152e9e8cfe9d59b2e0b70f4
Vishan Seru <vishanseru9@gmail.com>
2020-05-03 12:27:10 +0530

    Init commit.

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+val kotlinCoroutinesVersion = "1.3.5"
+
+plugins {
+  id("org.jetbrains.kotlin.js") version "1.3.72"
+}
+
+group = "com.bluesir9"
+version = "0.0.1"
+
+repositories {
+  maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
+  mavenCentral()
+  jcenter()
+}
+
+dependencies {
+  implementation(kotlin("stdlib-js"))
+
+  //region Coroutines
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:$kotlinCoroutinesVersion")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:$kotlinCoroutinesVersion")
+  //endregion
+}
+
+kotlin.target.browser { }
