85ed456e14723cbcb2e300f75617984b18e9594f
Vishan Seru <vishanseru9@gmail.com>
2020-07-11 22:51:35 +0530

    Implement a super simple logger.

---
+++ src/main/kotlin/logging/Logger.kt
@@ -0,0 +1,8 @@
+package logging
+
+interface Logger {
+  fun info(message: String)
+  fun debug(message: String)
+  fun debug(message: String, throwable: Throwable)
+  fun error(message: String, throwable: Throwable)
+}
