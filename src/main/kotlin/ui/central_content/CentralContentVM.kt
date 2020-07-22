831662f6f4683722a0947b6220969a1bc26fade7
Vishan Seru <vishanseru9@gmail.com>
2020-07-22 23:17:04 +0530

    Remove randomUUID setting in VM constructor since the VM id needs to be set based on the song id.

--- src/main/kotlin/ui/central_content/CentralContentVM.kt
+++ src/main/kotlin/ui/central_content/CentralContentVM.kt
@@ -1,14 +1,13 @@
 package ui.central_content
 
 import uuid.UUID
-import uuid.randomUUID
 
 sealed class CentralContentVM {
   object Loading : CentralContentVM()
   object Empty : CentralContentVM()
   data class SearchResults(val items: List<Item>): CentralContentVM() {
     data class Item(
-      val id: UUID = randomUUID(),
+      val id: UUID,
       val trackTitle: String,
       val trackArtist: String,
       val trackImageUrl: String
