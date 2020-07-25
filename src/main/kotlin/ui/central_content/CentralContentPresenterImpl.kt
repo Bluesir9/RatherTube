65e787a602b91f20793be642257ea6bf37e799be
Vishan Seru <vishanseru9@gmail.com>
2020-07-25 19:53:13 +0530

    Implement the entirety of the central content presenter.

--- src/main/kotlin/ui/central_content/CentralContentPresenterImpl.kt
+++ src/main/kotlin/ui/central_content/CentralContentPresenterImpl.kt
@@ -1,19 +1,51 @@
 package ui.central_content
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import logging.Logger
+import logging.LoggerImpl
+import song.SongPlaybackCoordinator
+import song.SongPlayer
 import ui.base.BasePresenterImpl
+import ui.central_content.SearchEvent.Loaded
+import ui.central_content.SearchEvent.Loading
 import uuid.UUID
 
-class CentralContentPresenterImpl : CentralContentContract.Presenter, BasePresenterImpl() {
+@ExperimentalCoroutinesApi
+class CentralContentPresenterImpl : CentralContentContract.Presenter, BasePresenterImpl<CentralContentContract.View>() {
+
+  private val logger: Logger = LoggerImpl(CentralContentPresenterImpl::class.simpleName!!)
+  private val searchEventsListener: SearchEventsListener = SearchResultsUICoordinator
+  private val generateVM: CentralContentVMGenerator = CentralContentVMGeneratorImpl()
+  private val songPlayer: SongPlayer = SongPlaybackCoordinator
+
+  private lateinit var lastSearchEvent: SearchEvent
+  private lateinit var lastVM: CentralContentVM
 
   override fun onStart() {
-    TODO("Not yet implemented")
+    searchEventsListener
+      .getSearchEvents()
+      .onEach { lastSearchEvent = it }
+      .map { generateVM(it) }
+      .onEach { lastVM = it }
+      .onEach { view.render(it) }
+      .launchIn(this)
   }
 
   override fun onSearchResultClick(id: UUID) {
-    TODO("Not yet implemented")
+    when(val searchEvent = lastSearchEvent) {
+      is Loading -> logger.error("Unexpected encounter of search result click event. lastVM = $lastVM")
+      is Loaded -> {
+        val clickedSong = searchEvent.songs.firstOrNull { song -> song.id == id }
+        if (clickedSong != null) songPlayer.playRequested(clickedSong)
+        else logger.error("Unable to find clicked song. lastVM = $lastVM, searchEvent = $searchEvent")
+      }
+    }
   }
 
   override fun onStop() {
-    TODO("Not yet implemented")
+    //no-op
   }
 }
