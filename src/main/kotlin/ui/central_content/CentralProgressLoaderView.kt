d12001b1ba728289a8d021efa159ed90bd853b39
Vishan Seru <vishanseru9@gmail.com>
2020-07-30 22:57:08 +0530

    Use setProperty API instead of setPropertyValue and add animation related CSS to hierarchy properly.

--- src/main/kotlin/ui/central_content/CentralProgressLoaderView.kt
+++ src/main/kotlin/ui/central_content/CentralProgressLoaderView.kt
@@ -4,8 +4,7 @@ import extensions.createHtmlElementWithClass
 import extensions.querySelectorAllAsHtmlElements
 import extensions.querySelectorAsHtmlElement
 import org.w3c.dom.HTMLElement
-import org.w3c.dom.asList
-import org.w3c.dom.css.CSSStyleSheet
+import org.w3c.dom.HTMLStyleElement
 import ui.base.Renderable
 import kotlin.browser.document
 
@@ -35,43 +34,36 @@ class CentralProgressLoaderView(
         style.width = "6px"
         style.display = "inline-block"
 
-        style.setPropertyValue("-webkit-animation", "sk-stretchdelay 1.2s infinite ease-in-out")
+        style.setProperty("-webkit-animation", "sk-stretchdelay 1.2s infinite ease-in-out")
         style.animation = "sk-stretchdelay 1.2s infinite ease-in-out"
       }
 
     document.querySelectorAsHtmlElement(".area_content_central_loader .rect2").style
       .apply {
-        setPropertyValue("-webkit-animation-delay", "-1.1s")
+        setProperty("-webkit-animation-delay", "-1.1s")
         animationDelay = "-1.1s"
       }
 
     document.querySelectorAsHtmlElement(".area_content_central_loader .rect3").style
       .apply {
-        setPropertyValue("-webkit-animation-delay", "-1.0s")
+        setProperty("-webkit-animation-delay", "-1.0s")
         animationDelay = "-1.0s"
       }
 
     document.querySelectorAsHtmlElement(".area_content_central_loader .rect2").style
       .apply {
-        setPropertyValue("-webkit-animation-delay", "-0.9s")
+        setProperty("-webkit-animation-delay", "-0.9s")
         animationDelay = "-0.9s"
       }
 
     document.querySelectorAsHtmlElement(".area_content_central_loader .rect2").style
       .apply {
-        setPropertyValue("-webkit-animation-delay", "-0.8s")
+        setProperty("-webkit-animation-delay", "-0.8s")
         animationDelay = "-0.8s"
       }
 
-    /*
-    FIXME:
-     I am not sure if below approach will work
-     but from my research, this is the only
-     mechanism via which we can insert animation
-     and keyframe related rules to CSS. 
-     */
-    val css = document.styleSheets.asList().first() as CSSStyleSheet
-    css.insertRule("""
+    val styleElement = document.createElement("style") as HTMLStyleElement
+    styleElement.innerText = """
       @-webkit-keyframes sk-stretchdelay {
         0%, 40%, 100% { -webkit-transform: scaleY(0.4) }  
         20% { -webkit-transform: scaleY(1.0) }
@@ -86,7 +78,8 @@ class CentralProgressLoaderView(
           -webkit-transform: scaleY(1.0);
         }
       }
-    """.trimIndent(), css.cssRules.length)
+    """.trimIndent()
+    document.head!!.appendChild(styleElement)
   }
 
   override fun onLayoutReady() {
