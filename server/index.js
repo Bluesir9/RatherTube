bc0100d1cf2a4d7ef52e7c68e298560932df8a7f
Vishan Seru <vishanseru9@gmail.com>
2020-08-15 21:08:15 +0530

    Breakup search API into 2 APIs. First for just returning search results without streaming information and second for returning the stream URL for a given video who's id has been provided.

--- server/index.js
+++ server/index.js
@@ -7,9 +7,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+//region GET search API
 app.get("/api/youtube/search", async (request, response) => {
   const query = request.query.query;
-  if(!query) {
+  if (!query) {
     const statusCode = 422;
     const errorJSON = {
       error_code: statusCode,
@@ -18,46 +19,65 @@ app.get("/api/youtube/search", async (request, response) => {
     response.status(statusCode).json(errorJSON);
   } else {
     const searchResults = await getSearchResults(query);
-    response.status(200).json(searchResults);
+    const responseJson = {
+      items: searchResults
+    }
+    response.status(200).json(responseJson);
   }
 });
 
 async function getSearchResults(query) {
   const searchResults = await searchYouTube(query);
-  const searchResultsWithInfo =
-    await Promise.all(
-      searchResults.map(result => getInfo(result))
-        //this filter removes any null, undefined objects from the array
-        .filter(x => x)
-    );
-
-  return {
-    items: searchResultsWithInfo
-  }
+  return searchResults.map(function (result) {
+      return {
+        video_id: result.id,
+        video_title: result.title,
+        video_artist: result.channel.name,
+        video_thumbnail: result.thumbnail,
+      }
+    });
 }
 
 async function searchYouTube(query) {
-  return youtubeScraper.search(query)
+  return youtubeScraper.search(query);
 }
 
-async function getInfo(searchResult) {
-  return new Promise(((resolve, reject) => {
-    ytdl.getInfo(searchResult.link, (error, info) => {
+//endregion
+
+//region GET stream API
+app.get("/api/youtube/stream", async (request, response) => {
+  const videoId = request.query.videoId;
+  if (!videoId) {
+    const statusCode = 422;
+    const errorJSON = {
+      error_code: statusCode,
+      error_message: "Illegal value of videoId argument"
+    }
+    response.status(statusCode).json(errorJSON);
+  } else {
+    const streamUrl = await getStreamUrl(videoId);
+    const responseJSON = {
+      stream_url: streamUrl
+    };
+    response.status(200).json(responseJSON);
+  }
+});
+
+async function getStreamUrl(videoId) {
+  const videoUrl = "https://www.youtube.com/watch?v=" + videoId;
+  return new Promise(((resolve) => {
+    ytdl.getInfo(videoUrl, (error, info) => {
       if (error) {
         console.error("Failed to extract info for link = " + searchResult.link, error);
         return resolve(null);
       } else {
-        const infoJson = {
-          video_id: info.id,
-          video_title: info.title,
-          video_thumbnail_url: info.thumbnail,
-          video_stream_url: info.url
-        }
-        return resolve(infoJson);
+        return resolve(info.url);
       }
     });
   }));
 }
+
+//endregion
 
 app.listen(PORT, () => {
   console.log("RatherTube server running at http://localhost:" + PORT);
