af7b47a8fdd19d917f2695891c13cdf6809037c1
Vishan Seru <vishanseru9@gmail.com>
2020-08-17 21:04:51 +0530

    Add couple of new environment variables that will be used by Ktor client to figure out where the server its supposed to hit is located.

--- src/main/kotlin/config/Environment.kt
+++ src/main/kotlin/config/Environment.kt
@@ -1,5 +1,18 @@
 package config
 
+import io.ktor.http.URLProtocol
+
 interface Environment {
   val debug: Boolean
+  val apiProtocol: URLProtocol
+  /*
+  As per expected implementation,
+  1. It should not contain the http/https
+  prefix since that is taken care of by the
+  `apiProtocol` Environment variable.
+  2. It should not end with a `/` suffix
+  since the individual API implementations
+  will define it themselves.
+  */
+  val host: String
 }
