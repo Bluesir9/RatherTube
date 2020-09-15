4ee3fc2f61ecf61fd22b12de55cafe5bb4ce197d
Vishan Seru <vishanseru9@gmail.com>
2020-09-15 20:55:17 +0530

    Add error messages that will be shown as part of the new audio playback implementation.

--- src/main/kotlin/config/StringResource.kt
+++ src/main/kotlin/config/StringResource.kt
@@ -6,4 +6,9 @@ object StringResource {
   const val NO_SEARCH_RESULTS_FOUND = "No search results found"
   const val NETWORK_EXCEPTION_ENCOUNTERED = "Network exception encountered"
   const val SOME_ERROR_ENCOUNTERED = "Some error encountered"
+  const val STREAM_LOAD_FAILED = "Failed to load stream"
+  const val PAUSE_FAILED_CAUSE_NO_TRACK_FOUND = "Cannot pause track cause no track was found"
+  const val RESUME_FAILED_CAUSE_NO_TRACK_FOUND = "Cannot resume track cause no track was found"
+  const val REWIND_FAILED_CAUSE_NO_TRACK_FOUND = "Cannot rewind track cause no track was found"
+  const val FORWARD_FAILED_CAUSE_NO_TRACK_FOUND = "Cannot go to next track cause no active track was found"
 }
