7d10d97b303b9bb713bc7c2152b8d8542743b5bd
Vishan Seru <vishanseru9@gmail.com>
2020-09-27 13:06:58 +0530

    Add new API to start playback of an item already present in the playback queue.

--- src/main/kotlin/playback/usecases/Play.kt
+++ src/main/kotlin/playback/usecases/Play.kt
@@ -4,6 +4,7 @@ import playback.player.YouTubeVideoPlayer
 import playback.player.YouTubeVideoPlayerImpl
 import playback.queue.PlaybackQueue
 import playback.queue.PlaybackQueueImpl
+import playback.queue.PlaybackQueueItem
 import youtube.YouTubeVideo
 
 class Play {
@@ -15,5 +16,16 @@ class Play {
     playbackQueue.clear()
     val playbackQueueItem = playbackQueue.add(video)
     player.play(playbackQueueItem)
+  }
+
+  operator fun invoke(playbackQueueItem: PlaybackQueueItem) {
+    /*
+    TODO:
+      The song is already present in the queue and the
+      user wants to play it directly. This should not
+      alter the queue in any shape and form and simply
+      begin playing the song being pointed to by this
+      playback queue item.
+    */
   }
 }
