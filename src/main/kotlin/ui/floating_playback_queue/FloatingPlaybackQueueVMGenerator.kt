c3390d3ea4497443832e91b4d50c422a74434473
Vishan Seru <vishanseru9@gmail.com>
2020-09-27 13:09:13 +0530

    Create basic floating playback queue vm generator class.

---
+++ src/main/kotlin/ui/floating_playback_queue/FloatingPlaybackQueueVMGenerator.kt
@@ -0,0 +1,43 @@
+package ui.floating_playback_queue
+
+import config.Color
+import playback.queue.PlaybackQueueItem
+
+interface FloatingPlaybackQueueVMGenerator {
+  /*
+  `queue` can be an empty list.
+   */
+  operator fun invoke(currentVM: FloatingPlaybackQueueVM, queue: List<PlaybackQueueItem>): FloatingPlaybackQueueVM
+}
+
+class FloatingPlaybackQueueVMGeneratorImpl : FloatingPlaybackQueueVMGenerator {
+
+  override fun invoke(currentVM: FloatingPlaybackQueueVM, queue: List<PlaybackQueueItem>): FloatingPlaybackQueueVM =
+    FloatingPlaybackQueueVM(
+      visible = currentVM.visible,
+      clearButtonEnabled = queue.isNotEmpty(),
+      items = queue.mapIndexed { index, queueItem -> getItemVMs(index, queueItem) }
+    )
+
+  private fun getItemVMs(index: Int, queueItem: PlaybackQueueItem): FloatingPlaybackQueueVM.Item =
+    FloatingPlaybackQueueVM.Item(
+      id = queueItem.id,
+      backgroundColor = getBackgroundColor(index),
+      trackTitle = queueItem.video.title,
+      trackArtist = queueItem.video.artist,
+      thumbnailUrl = queueItem.video.thumbnailUrl
+    )
+
+  /*
+  FIXME:
+    The actual player will need to update
+    the playback queue to indicate which item
+    is actively being played so that we can
+    implement this function more accurately
+    to indicate the queue item which is being
+    played actively.
+   */
+  private fun getBackgroundColor(index: Int): String =
+    if(index == 0) Color.BACKGROUND_PLAYBACK_QUEUE_ITEM_ACTIVELY_PLAYING
+    else Color.BACKGROUND_PLAYBACK_QUEUE_ITEM_NOT_ACTIVELY_PLAYING
+}
