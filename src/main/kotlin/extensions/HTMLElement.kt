1456798bf4e341b5d99e11face54da81137f558e
Vishan Seru <vishanseru9@gmail.com>
2020-09-28 23:42:57 +0530

    Add extension functions to get first matching html element by class name and to make code related to showing and hiding html views reusable.

--- src/main/kotlin/extensions/HTMLElement.kt
+++ src/main/kotlin/extensions/HTMLElement.kt
@@ -6,9 +6,25 @@ import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import org.w3c.dom.HTMLElement
+import org.w3c.dom.get
 
 fun HTMLElement.clickEventsAsFlow(): Flow<Unit> {
   val channel = BroadcastChannel<Unit>(1)
   this.onclick = { channel.offer(Unit) }
   return channel.asFlow()
+}
+
+fun HTMLElement.getFirstHTMLElementByClassName(className: String): HTMLElement =
+  this.getElementsByClassName(className)[0] as HTMLElement
+
+enum class StyleDisplay(val value: String) {
+  Flex("flex")
+}
+
+fun HTMLElement.show(styleDisplay: StyleDisplay) {
+  this.style.display = styleDisplay.value
+}
+
+fun HTMLElement.hide() {
+  this.style.display = "none"
 }
