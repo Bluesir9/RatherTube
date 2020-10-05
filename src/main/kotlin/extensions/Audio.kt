b8e71363311e472061fb89190180b505aea76a14
Vishan Seru <vishanseru9@gmail.com>
2020-10-05 23:15:06 +0530

    Add extension function to get totalLength from Audio object.

--- src/main/kotlin/extensions/Audio.kt
+++ src/main/kotlin/extensions/Audio.kt
@@ -4,3 +4,5 @@ import com.soywiz.klock.TimeSpan
 import org.w3c.dom.Audio
 
 val Audio.playedLength get() = TimeSpan(this.currentTime * 1000)
+
+val Audio.totalLength get() = TimeSpan(this.duration * 1000)
